d0686274f5c2d0ca6ca5a4f4510c6a89654f998e
Aakriti Suman <37480700+aakritisuman0@users.noreply.github.com>
2019-03-16 11:56:52 +0530

    Add files via upload

---
+++ helmet_client_final.ino
@@ -0,0 +1,137 @@
+#include <WiFi.h>
+#include <Wire.h>
+
+#include <RTClib.h>
+
+
+// your network name also called SSID
+char ssid[] = "Aakriti";
+// your network password
+char password[] = "aakriti143";
+// your network key Index number (needed only for WEP)
+int keyIndex = 0;
+
+uint16_t port = 23;     // port number of the server//TCP, UDP with telnet protocol: encrypted text communication
+IPAddress server(192,168,43,224);   // IP Address of the server
+WiFiClient client;
+
+#define alcPin 31
+#define irPin 32
+#define buzPin 10
+int alc,vib,flag=0;
+RTC_Millis RTC;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(alcPin,INPUT);
+  pinMode(irPin,INPUT);
+  pinMode(buzPin,OUTPUT);
+  pinMode(GREEN_LED,OUTPUT);
+
+ Serial.print("Attempting to connect to Network named: ");
+  // print the network name (SSID);
+  Serial.println(ssid); 
+  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED)
+  {
+    // print dots while we wait to connect
+    Serial.print(".");
+    delay(300);
+  }
+
+  Serial.println("\nYou're connected to the network");
+ Serial.println("Waiting for an ip address");
+  
+  while (WiFi.localIP() == INADDR_NONE) {
+    // print dots while we wait for an ip addresss
+    Serial.print(".");
+    delay(300);
+  }
+
+  Serial.println("\nIP Address obtained");
+  
+  // you're connected now, so print out the status  
+  printWifiStatus();
+
+  
+  // attempt to connect to the server
+  Serial.println("Attempting to connect to server");
+
+uint8_t tries = 0;
+  while (client.connect(server, port) == false)
+  {
+    Serial.print(".");
+    if (tries++ > 100) {
+      Serial.println("\nThe server isn't responding");
+      while(1);
+    }
+    delay(100);
+  }
+  
+  //we've connected to the server by this point
+  Serial.println("\nConnected to the server!");
+
+
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("Network Name: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+
+}
+
+void loop()
+{
+  alc=digitalRead(alcPin);
+  int ir=digitalRead(irPin);
+  
+  if(ir==1 && alc==HIGH)
+  {
+    digitalWrite(GREEN_LED,HIGH);
+    digitalWrite(RED_LED,LOW);
+    digitalWrite(buzPin,LOW);
+    int buffer1=1;
+   
+    //send the serial command to the server
+    client.println(buffer1);
+  
+    Serial.print("Sent: ");
+    Serial.println(buffer1);
+  }
+  else
+  {
+    digitalWrite(GREEN_LED,LOW);
+    digitalWrite(RED_LED,HIGH);
+    digitalWrite(buzPin,HIGH);
+    int buffer1=0;
+   
+    //send the serial command to the server
+    client.println(buffer1);
+    Serial.print("Sent: ");
+    Serial.println(buffer1);
+  }
+  delay(300);
+
+  if (client.available())
+  {
+    char buffer[255] = {0};
+    client.read((uint8_t*)buffer, client.available());
+    Serial.print("Received: ");
+    Serial.println(buffer);
+}
+}
---
+++ helmet_server_final.ino
@@ -0,0 +1,162 @@
+#include <WiFi.h>
+#include <RTClib.h>
+char thingSpeakAddress[] = "api.thingspeak.com";
+String writeAPIKey = "6TD3SKH61R1RCL0N";
+char buffer[255];
+RTC_Millis RTC;
+WiFiClient clients;  //for thingspeak
+WiFiClient client; 
+WiFiClient client2;
+float conval1;
+boolean alreadyConnected = false;
+boolean alreadyConnected2 = false;
+char ssid[] = "Anda";
+char password[] = "hello123";
+int motion1=0;
+int motion2=0;
+WiFiServer port(23);
+//WiFiServer port2(25);
+
+boolean lastConnected = false;                 // state of the connection last time through the main loop
+int failedCounter = 0;
+
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  RTC.begin(DateTime(__DATE__, __TIME__));
+  Serial.print("Attempting to connect to Network named: ");
+  Serial.println(ssid); 
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nYou're connected to the network");
+  Serial.println("Waiting for an ip address");  
+  while (WiFi.localIP() == INADDR_NONE) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println("\nIP Address obtained");
+  printWifiStatus();
+  port.begin();
+  //port2.begin();
+  pinMode(31,OUTPUT);
+}
+void loop() {
+  DateTime now = RTC.now();
+    
+    client = port.available();
+    if (client) {
+      if(!alreadyConnected) {
+      // clear out the input buffer:
+      client.flush();
+      Serial.println("We have a new client from port1");
+      client.println("Hello, client1!");
+      
+      alreadyConnected = true;
+    }
+    if (client.available() > 0) {
+      // read the bytes incoming from the client:
+      char buffer1[1];
+      client.read((uint8_t*)buffer1, client.available());
+       Serial.println("Recieved");
+       Serial.println(buffer1);
+       if(buffer1[0]=='1')
+       {
+        digitalWrite(31,HIGH);
+       }
+       else if(buffer1[0]=='0')
+       {
+        digitalWrite(31,LOW);
+       }
+      // updateThingSpeak("field2="+String(buffer1));
+      
+           
+          }
+    }
+
+
+   /*client2 = port2.available(); 
+    if (client2) {
+      if (!alreadyConnected2) {
+      // clear out the input buffer:
+      client2.flush();
+      Serial.println("We have a new client from port2");
+      client2.println("Hello, client!");
+      alreadyConnected2 = true;
+      }  
+      if (client2.available() > 0) {
+      // read the bytes incoming from the client:
+      char thisChar = client2.read();
+      char buffer[255] = {0};
+      client2.read((uint8_t*)buffer, client2.available());
+      float conval = atof(buffer);
+      motion2 += conval;
+      Serial.print("Port 2 client data: ");
+      Serial.println(motion2);   
+      }
+     
+      }*/
+    
+}
+
+
+/*void updateThingSpeak(String tsData)
+{
+   Serial.println("Date string: " + tsData);
+    
+    Serial.println("...Connecting to Thingspeak");
+    
+    // Connecting and sending data to Thingspeak
+    if(clients.connect("api.thingspeak.com", 80))
+    {
+        Serial.println("...Connection succesful, updating datastreams");
+        
+        clients.print("POST /update HTTP/1.1\n");
+        clients.print("Host: api.thingspeak.com\n");
+        clients.print("Connection: close\n");
+        clients.print("X-THINGSPEAKAPIKEY: "+writeAPIKey+"\n");
+        clients.print("Content-Type: application/x-www-form-urlencoded\n");
+        clients.print("Content-Length: ");
+        clients.print(tsData.length());
+        clients.print("\n\n");
+        
+        clients.println(tsData); //the ""ln" is important here.
+    
+        // This delay is pivitol without it the TCP client will often close before the data is fully sent
+        delay(200);
+        
+        Serial.println("Thingspeak update sent.");
+    }
+    else{
+        // Failed to connect to Thingspeak
+        Serial.println("Unable to connect to Thingspeak.");
+    }
+    
+    if(!clients.connected()){
+        clients.stop();
+    }
+    clients.flush();
+    clients.stop();
+}
+
+*/
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm"); 
+}
+
